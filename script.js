9233e6dd7466ad1bdc35e375d2e8a5f87dd01002
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-02-05 10:33:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,29 +13,29 @@
       });*/
       var scene = new WebScene({
         portalItem:{
-         id:"fbbbaa2fbfda41b8b3f96427c3ac5c79" 
+         id:"8046207c1c214b5587230f5e5f8efc77" 
         }
       });
       
       var camera = new Camera({
-        position: [
-          -90.1952, // lon
-          38.6526, // lat
-          5000000// elevation in meters
+       position: [
+           -71.062146,
+          42.366198,
+          700// elevation in meters
         ],
         tilt:0,
         heading: 0
       })
       
       var camera2 = new Camera({
-        position: {
-          x: 116.4074,
-          y: 39.9042,
-          z: 5000000
-        },
-        tilt: 0,
+       position: [
+           -71.0994065,
+          42.3466803,
+          700// elevation in meters
+        ],
+        tilt:0,
         heading: 0
-      });
+      })
 
       var view = new SceneView({
         container: "viewDiv",
@@ -60,19 +60,19 @@
       // Add the home button to the top left corner of the view
     view.ui.add(homeBtn, "top-left");
     
-    [stl, bei].forEach(function(button) {
+    [td_garden,fen_park].forEach(function(button) {
       button.style.display = 'flex';
       view.ui.add(button, 'top-right');
     });
     
-    bei.addEventListener('click', function() {
+    fen_park.addEventListener('click', function() {
       // reuse the default camera position already established in the homeBtn
       view.goTo({
         target:camera2
       });
     });
     
-    stl.addEventListener('click', function() {
+    td_garden.addEventListener('click', function() {
       // reuse the default camera position already established in the homeBtn
       view.goTo({
         target:camera
